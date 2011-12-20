1e256644c058ef905e9f901573084251f59d264f
DanX <danx.exe@gmail.com>
2011-12-20 17:28:30 -0200

    keep trying to find a paradigm by shortening the sufix

--- lib/conjugue.rb
+++ lib/conjugue.rb
@@ -18,19 +18,37 @@ module Conjugue
 
     paradigm = (VerbDatabase.find_by_verb(name) or VerbDatabase.find_by_paradigm(name) or VerbDatabase.find_by_sufix(name))
     
-    unless paradigm[:conjugations][time]
+    # Try finding paradigm by sufix
+    unless (paradigm[:conjugations][time] rescue nil)
       puts 'time not found'
       paradigm = VerbDatabase.find_by_sufix(name)
+
+      # Try shortening the sufix
+      unless (paradigm[:conjugations][time] rescue nil)
+        if (paradigm[:sufix] rescue nil)
+          while (sufix = paradigm[:sufix].to_s[1..-1]).size > 1
+            paradigm = VerbDatabase.find_by_sufix(sufix) rescue nil
+            break if paradigm
+          end
+        end
+      end
+
     end
 
     if paradigm
-      sufix = (paradigm[:sufix].to_s.empty? ? paradigm[:name] : paradigm[:sufix])
-      conjugate = paradigm[:conjugations][time][person]
-      radical_paradigm = paradigm[:name].gsub(Regexp.new(sufix+'$'), '')
-      sufix_conjugate = conjugate.gsub(Regexp.new('^'+radical_paradigm), '')
-      radical_verb = name.gsub(Regexp.new(sufix+'$'), '')
+      begin
+        sufix = (paradigm[:sufix].to_s.empty? ? paradigm[:name] : paradigm[:sufix])
+        conjugate = paradigm[:conjugations][time][person]
+        radical_paradigm = paradigm[:name].gsub(Regexp.new(sufix+'$'), '')
+        sufix_conjugate = conjugate.gsub(Regexp.new('^'+radical_paradigm), '')
+        radical_verb = name.gsub(Regexp.new(sufix+'$'), '')
 
-      conjugate = radical_verb + sufix_conjugate
+        conjugate = radical_verb + sufix_conjugate
+      rescue
+        nil
+      end
+    else
+      conjugate = name
     end
 
     ret = "#{VerbDatabase::PRONOUNS[person]} #{conjugate}"
